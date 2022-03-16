48306d35ab16608b2ac5f21dd85056b2d778e3f0
M.Ilyes <messal.ilyes@outlook.fr>
2022-03-16 12:00:31 +0100

    Quasi finalisation du Projet_1, finir CSS

---
+++ src/main/java/premiereVue/PremiersElementsGraphique.java
@@ -0,0 +1,25 @@
+package premiereVue;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class PremiersElementsGraphique extends Application {
+    public void start (Stage stage){
+        VBox root = new VBox(10);
+        Label labelHello = new Label("Hello");
+        Label labelHelloBis = new Label("Hello JavaFX");
+
+        root.getChildren().addAll(labelHello, labelHelloBis);
+        Scene scene = new Scene(root, 300, 80);
+        stage.setScene(scene);
+        stage.setTitle("Hello JavaFX");
+        stage.show();
+        }
+    public static void main ( String[] args) {
+        Application.launch();
+
+    }
+}
